967cb7c789d452af65ba36f3852b6da54a6ea3dd
Artur0HM90 <arturohurtadomendez90@gmail.com>
2023-11-26 17:12:06 -0500

    ejerciio 5 de if

---
+++ if/5 cinco.js
@@ -0,0 +1,10 @@
+/* 5 - Escribe un programa que determine si un número es divisible por 5. */
+
+let ingresaNumero = prompt ("Ingresa un número.");
+
+if (ingresaNumero % 5 == 0) {
+    console.log("El número " + ingresaNumero + " si es divisible por 5.");
+
+}else{
+    console.log("El número " + ingresaNumero + " no es divisible por 5.")
+}

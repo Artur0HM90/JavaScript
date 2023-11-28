6d7d683f49957d004494454d1cf84d949f70c07a
Artur0HM90 <arturohurtadomendez90@gmail.com>
2023-11-28 10:25:52 -0500

    ejerciio 5 de switch

---
+++ switch/5 cincoSwitch.js
@@ -0,0 +1,40 @@
+/* 
+5 - Estación del año:
+
+Solicitar al usuario que ingrese un mes.
+Utilizar un switch para imprimir la estación del año correspondiente.
+
+*/
+
+let ingresaMes = prompt ("Ingresa mes:");
+ingresaMes = ingresaMes.toUpperCase();
+
+switch (ingresaMes) {
+    case "DICIEMBRE":
+    case "ENERO": 
+    case "FEBRERO":
+    case "MARZO":
+        document.write("El mes de " + ingresaMes + " es Verano.");
+        break;
+    
+    case "ABRIL": 
+    case "MAYO":
+    case "JUNIO":
+        document.write("El mes de " + ingresaMes + " es Otoño.");
+        break;
+    
+    case "JULIO":
+    case "AGOSTO":
+    case "SETIEMBRE":
+        document.write("El mes de " + ingresaMes + " es Invierno.");
+        break;
+
+    case "OCTUBRE":
+    case "NOVIEMBRE":
+        document.write("El mes de " + ingresaMes + " es Primavera."); 
+        break;
+
+    default:
+        document.write("Debes ingresa una estación.");
+        break;
+}

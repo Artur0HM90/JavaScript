816b529f0fd55f348adc9d7f5275893db80ae3dc
Artur0HM90 <arturohurtadomendez90@gmail.com>
2023-11-29 10:13:22 -0500

    ejerciio 1 de function

--- funciones/1 unoFunction.js
+++ funciones/1 unoFunction.js
@@ -1,15 +1,3 @@
-function estoEsUnaFuncion(params) {
-    console.log("Uno");
-    return 19;
-    console.log("Dos");
-    console.log("Tres");
-    return "La funcion ha retornado una cadena de texto.";
-}
-/*
-let valorDeFuncion = estoEsUnaFuncion();
-console.log(valorDeFuncion);
-*/
-
 let nombre = prompt("Ingresa tu nombre: ");
 let edad = prompt ("Ingresa tu edad: ");
 
@@ -18,8 +6,6 @@ edad = parseInt(edad);
 
 function saludar(nombre, edad) {
     console.log(`Hola mi nombre es ${nombre} y tengo ${edad} años.`);
-
-
 }
 saludar(nombre, edad);
 
